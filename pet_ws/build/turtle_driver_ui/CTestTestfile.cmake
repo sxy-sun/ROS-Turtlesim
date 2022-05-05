66dd5c2e080451e164ed41e0680095e6d2b679bf
Shaw <sunxycn98@gmail.com>
2022-05-05 11:34:47 -0400

    removed the entire catkin workspace only kept two packages

--- pet_ws/build/turtle_driver_ui/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bedestrian/ROS-Turtlesim/pet_ws/src/turtle_driver_ui
-# Build directory: /home/bedestrian/ROS-Turtlesim/pet_ws/build/turtle_driver_ui
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
