71682958504775a9d27cd41f99bd38573f0b07ae
Xiaoyu Sun <49962634+sxy-sun@users.noreply.github.com>
2022-05-05 12:27:56 -0400

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/pet_ws/src
 cd ~/pet_ws/src
 git clone https://github.com/sxy-sun/ROS-Turtlesim.git
+
+# Move everything under src
+shopt -s dotglob
+mv ~/pet_ws/src/ROS-Turtlesim/* ~/pet_ws/src/
+rm -r ~/pet_ws/src/ROS-Turtlesim/
+
 cd ~/pet_ws
 catkin_make
 
